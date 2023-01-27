3b6b1b3a5d54a99d875d4079f7841ad0bef5968c
Yuuki Takao <takaaaao3@gmail.com>
2023-01-27 18:19:20 +0900

    abc142 a, d

---
+++ abc142/a/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Buffer(make([]byte, 128), 500000)
+	sc.Split(bufio.ScanWords)
+	n := scanInt()
+	odd := 0
+	for i := 1; i <= n; i++ {
+		if i%2 == 1 {
+			odd++
+		}
+	}
+	ans := float64(odd) / float64(n)
+	fmt.Printf("%.10f\n", ans)
+}
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	return atoi(sc.Text())
+}
+func atoi(nStr string) int {
+	i, err := strconv.Atoi(nStr)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+func scanText() string {
+	sc.Scan()
+	return sc.Text()
+}
