12f931752c123228b20a58439fec284bf8edea01
Yuuki Takao <takaaaao3@gmail.com>
2023-02-26 18:08:19 +0900

    toita

---
+++ abc226/b/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Buffer(make([]byte, 128), 500000)
+	sc.Split(bufio.ScanWords)
+	n := scanInt()
+	// fmt.Printf("%d\n", n)
+
+	m := make(map[string]int)
+	for i := 0; i < n; i++ {
+		l := scanInt()
+		a := scanInts(l)
+		m[fmt.Sprint(a)]++
+	}
+	fmt.Printf("%d\n", len(m))
+}
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = scanInt()
+	}
+	return a
+}
+func scanInt() int {
+	sc.Scan()
+	return atoi(sc.Text())
+}
+func atoi(nStr string) int {
+	i, err := strconv.Atoi(nStr)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+func scanText() string {
+	sc.Scan()
+	return sc.Text()
+}
