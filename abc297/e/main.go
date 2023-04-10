61a835f9fa80dfb483b4d2e87cc04a6d4e2ffe02
Yuuki Takao <takaaaao3@gmail.com>
2023-04-10 11:34:07 +0900

    toita

---
+++ abc297/e/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	bufInit()
+	defer wr.Flush()
+	n := scanInt()
+	k := scanInt()
+
+	a := scanInts(n)
+	sort.Ints(a)
+
+	dp := make([]int, k+1)
+	dp[0] = a[0]
+
+	for i := 1; i < k; i++ {
+		for j := 0; j < n; j++ {
+			dp[i] = minOf(a[j+1], dp[i-1]+a[j])
+		}
+		fprintf("%d %d\n", i, dp[i])
+	}
+}
+func minOf(vars ...int) int {
+	min := int(math.Pow10(18))
+	for _, v := range vars {
+		if min > v {
+			min = v
+		}
+	}
+	return min
+}
+
+var wr *bufio.Writer
+var sc = bufio.NewScanner(os.Stdin)
+
+func fprintf(f string, a ...interface{}) {
+	fmt.Fprintf(wr, f, a...)
+}
+func fprintln(a ...interface{}) {
+	fmt.Fprintln(wr, a...)
+}
+func fprint(f string, a ...interface{}) {
+	fmt.Fprint(wr, a...)
+}
+func bufInit() {
+	sc.Buffer(make([]byte, 128), 500000)
+	sc.Split(bufio.ScanWords)
+	wr = bufio.NewWriter(os.Stdout)
+}
+func scanInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = scanInt()
+	}
+	return a
+}
+func scanInts2(n int) ([]int, []int) {
+	a, b := make([]int, n), make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i], b[i] = scanInt(), scanInt()
+	}
+	return a, b
+}
+func scanInt() int {
+	sc.Scan()
+	return atoi(sc.Text())
+}
+func scanFloat() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+func atoi(nStr string) int {
+	i, err := strconv.Atoi(nStr)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+func scanText() string {
+	sc.Scan()
+	return sc.Text()
+}
+func scanBytes() []byte {
+	sc.Scan()
+	return sc.Bytes()
+}
