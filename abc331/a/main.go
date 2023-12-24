836cb2ecb4f835bab14b47b13187b80f09c5b278
Yuuki Takao <takaaaao3@gmail.com>
2023-12-24 11:07:19 +0900

    toita

---
+++ abc331/a/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	bufInit()
+	defer wr.Flush()
+	M := scanInt()
+	D := scanInt()
+	y := scanInt()
+	m := scanInt()
+	d := scanInt()
+
+	isNextMonth := false
+	dd := d
+	if (dd + 1) == D+1 {
+		dd = 1
+		isNextMonth = true
+	} else {
+		dd = (d + 1)
+	}
+	md := m
+	isNextYear := false
+	if isNextMonth {
+		md++
+		if md == M+1 {
+			md = 1
+			isNextYear = true
+		}
+	}
+	yd := y
+	if isNextYear {
+		yd++
+	}
+
+	fprintf("%d %d %d\n", yd, md, dd)
+}
+
+var wr *bufio.Writer
+var sc = bufio.NewScanner(os.Stdin)
+
+func fprintf(f string, a ...interface{}) {
+	fmt.Fprintf(wr, f, a...)
+}
+func fprintln(a ...interface{}) {
+	fmt.Fprintln(wr, a...)
+}
+func fprint(f string, a ...interface{}) {
+	fmt.Fprint(wr, a...)
+}
+func bufInit() {
+	const startBufSize = 4096
+	const maxScanTokenSize = math.MaxInt64
+	sc.Buffer(make([]byte, startBufSize), maxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+	wr = bufio.NewWriter(os.Stdout)
+}
+func scanInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = scanInt()
+	}
+	return a
+}
+func scanInts2(n int) ([]int, []int) {
+	a, b := make([]int, n), make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i], b[i] = scanInt(), scanInt()
+	}
+	return a, b
+}
+func scanInt() int {
+	sc.Scan()
+	return atoi(sc.Text())
+}
+func scanFloat() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+func atoi(nStr string) int {
+	i, err := strconv.Atoi(nStr)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+func scanText() string {
+	sc.Scan()
+	return sc.Text()
+}
+func scanBytes() []byte {
+	sc.Scan()
+	return sc.Bytes()
+}
