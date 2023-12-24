ed3971c8b3ac1cb6d4e415960428f77d365d20e2
Yuuki Takao <takaaaao3@gmail.com>
2023-12-24 18:00:02 +0900

    toita

--- abc331/c/main.go
+++ abc331/c/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -12,30 +13,34 @@ func main() {
 	bufInit()
 	defer wr.Flush()
 	n := scanInt()
-	s := scanInt()
-	m := scanInt()
-	l := scanInt()
-	// fprintf("%d %d %d %d\n", n, s, m, l)
-	ans := int(math.Pow10(18))j
-	for i := 0; i < 18; i++ {
-		for j := 0; j < 14; j++ {
-			for k := 0; k < 9; k++ {
-				if 6*i+8*j+12*k >= n {
-					ans = minOf(ans, i*s+j*m+k*l)
-				}
-			}
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = scanInt()
+	}
+	counts := make(map[int]int)
+	for _, v := range a {
+		counts[v]++
+	}
+	ans := make(map[int]int)
+	reversed := deepCopySlice(a)
+	sort.Sort(sort.Reverse(sort.IntSlice(reversed)))
+	now := 0
+	for _, v := range reversed {
+		cnt := counts[v]
+		if _, ok := ans[v]; !ok {
+			ans[v] = now
+			now += v * cnt
 		}
 	}
-	fprintln(ans)
+	for _, v := range a {
+		fprintf("%d ", ans[v])
+	}
 }
-func minOf(vars ...int) int {
-	min := int(math.Pow10(18))
-	for _, v := range vars {
-		if min > v {
-			min = v
-		}
-	}
-	return min
+
+func deepCopySlice(s []int) []int {
+	tmp := make([]int, len(s))
+	copy(tmp, s)
+	return tmp
 }
 
 var wr *bufio.Writer
